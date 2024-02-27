3e72fb38cb1aabb786868e10101978aee924f54a
GoodpastureR <eggboy06@gmail.com>
2024-02-27 09:58:43 -0700

    2/27 added a test file and a speech recognition lib

---
+++ transcribe.py
@@ -0,0 +1,51 @@
+"""
+Program Name: transcribe.py
+Program Purpose: To transcribe the video footage from Dr.Heldman's class
+Programmer: Ruby Goodpasture
+Date: 2/27/2024
+"""
+
+import pyaudio
+import speech_recognition as sr
+import pyttsx3
+import os
+
+
+def SpeakText(command):
+     
+    # Initialize the engine
+    engine = pyttsx3.init()
+    engine.say("peetah") 
+    engine.runAndWait()
+
+SpeakText("1")
+
+#while(1):    
+     
+    # Exception handling to handle
+    # exceptions at the runtime
+try:
+    audio = os.open("testfiles/Recording.m4a", os.O_RDWR) 
+    # use the microphone as source for input.
+    #with sr.Microphone() as source2:
+             
+        # wait for a second to let the recognizer
+        # adjust the energy threshold based on
+        # the surrounding noise level 
+        #sr.adjust_for_ambient_noise(source2, duration=0.2)
+             
+        #listens for the user's input 
+        #audio2 = sr.listen(source2)
+             
+        # Using google to recognize audio
+    MyText = sr.recognizer_instance.recognize_google(audio)
+    MyText = MyText.lower()
+ 
+    print("Did you say ",MyText)
+    SpeakText(MyText)
+             
+except sr.RequestError as e:
+    print("Could not request results; {0}".format(e))
+         
+except sr.UnknownValueError:
+    print("unknown error occurred")
